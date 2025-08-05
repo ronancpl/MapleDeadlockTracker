4eb8c8b8fb502fd2c7a52720c260423afed4a505
Ronan Lana <rcpl2010@gmail.com>
2025-08-05 13:27:38 -0300

    Update License

--- src/mapledeadlocktracker/containers/MapleDeadlockClass.java
+++ src/mapledeadlocktracker/containers/MapleDeadlockClass.java
@@ -1,8 +1,14 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    This file is part of the MapleQuestAdvisor planning tool
+    Copyleft (L) 2025 RonanLana
+
+    GNU General Public License v3.0
+
+    Permissions of this strong copyleft license are conditioned on making available complete
+    source code of licensed works and modifications, which include larger works using a licensed
+    work, under the same license. Copyright and license notices must be preserved. Contributors
+    provide an express grant of patent rights.
+*/
 package mapledeadlocktracker.containers;
 
 /**
